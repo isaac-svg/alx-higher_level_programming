2beb16cc5792ef2005067d293bd0b9375e26ef53
isaac-svg <isaacsakyi009@gmail.com>
2023-10-09 20:43:25 +0000

    data structures

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    new_string = ""
+    for i in range(len(my_string)):
+        char = my_string[i]
+        if char != 'c' and char != 'C':
+            new_string+=char
+    return new_string
