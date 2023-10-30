230621e36fe60bd09c49f27a399a2fe324deadde
isaac-svg <isaacsakyi009@gmail.com>
2023-10-30 12:17:16 +0000

    :first commit

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""Defines a Rectangle class."""
+
+class Rectangle:
+    """Represent a rectangle."""
+    def __init__(self, width=0, height=0):
+        """Initialize a Rectangle object.
+
+        Args:
+            width (int): The width of the rectangle object.
+            height (int): The height of the rectangle object.
+        """
+        self.width = width
+        self.height = height
+
+
+    @property
+    def width(self):
+        """set and get the width of the rectangle."""
+        return self.__width
+    
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    @property
+    def height(self):
+        """set and get the height of the rectangle."""
+        return self.__height
+    
