2a8909b7a98c65c39256424c1fc37efadcd1270e
isaac-svg <isaacsakyi009@gmail.com>
2023-10-31 23:09:15 +0000

    Objects

--- 0x09-python-everything_is_object/script.js
+++ 0x09-python-everything_is_object/script.js
@@ -1,7 +1,7 @@
 const  fs  = require("fs");
 (()=>{
 
-    for (let i = 11; i <= 28 ; i++) {
+    for (let i = 0; i <= 28 ; i++) {
         // <i know  this is a bad way to create files in a loop the callback will add up to the event loop increasing memory usesage
         fs.open(`./${i}-answer.txt`, "w", ()=>{}) 
         
