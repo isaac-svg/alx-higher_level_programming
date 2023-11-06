f4ddc7964bbe0c5055789d55acd8a2d13195260d
isaac-svg <isaacsakyi009@gmail.com>
2023-11-06 23:56:26 +0000

    Inheritance

---
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Defines a function that writes attributes to objects."""
+
+
+def add_attribute(obj, att, value):
+    """Add a new attribute to an object if writable.
+
+    Args:
+        obj (any): The object to add an attribute to.
+        att (str): The name of the attribute.
+        value (any): The value of att parameter.
+    Raises:
+        TypeError: If the attribute cannot be added.
+    """
+    if not hasattr(obj, "__dict__"):
+        raise TypeError("can't add new attribute")
+    setattr(obj, att, value)
