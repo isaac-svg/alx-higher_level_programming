bd526565940b3296e4c8977130ea378d0000db60
isaac-svg <isaacsakyi009@gmail.com>
2023-11-07 23:35:47 +0000

    I/O

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 """Defines a function that reads a file
- and writes the content to stdout"""
+ and writes the content"""
 
 
 def read_file(filename=""):
-    """reads from a file and prints it to stdout"""
+    """reads from a file and prints"""
     with open(filename, "r") as file:
         for f in file:
             print(f)
