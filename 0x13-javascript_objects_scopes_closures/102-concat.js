3c66af7e6e2ef4cc0957b6c820d7fd9e58ceb5ef
isaac-svg <isaacsakyi009@gmail.com>
2024-01-09 08:22:46 -0500

    objects

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const fs = require('fs');
 
-const Arg1 = fs.readFileSync(process.argv[2]).toString();
-const Arg2 = fs.readFileSync(process.argv[3]).toString();
+const Arg1 = fs.readFileSync(process.argv[2], 'utf8').toString();
+const Arg2 = fs.readFileSync(process.argv[3], 'utf8').toString();
 fs.writeFileSync(process.argv[4], Arg1 + Arg2);
