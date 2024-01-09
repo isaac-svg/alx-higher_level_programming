1a94f674de2ca48740e7fe7d34008e3457329b20
isaac-svg <isaacsakyi009@gmail.com>
2024-01-09 08:06:43 -0500

    objects

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,19 +1,10 @@
 #!/usr/bin/node
-const SquareP = require('./-square');
-
-class Square extends SquareP {
+module.exports = class Square extends require('./5-square.js') {
   charPrint (c) {
     if (c === undefined) {
-      c = 'X';
-    }
-    for (let i = 0; i < this.height; i++) {
-      let s = '';
-      for (let j = 0; j < this.width; j++) {
-        s += c;
-      }
-      console.log(s);
+      this.print();
+    } else {
+      for (let i = 0; i < this.height; i++) console.log(c.repeat(this.width));
     }
   }
-}
-
-module.exports = Square;
+};
