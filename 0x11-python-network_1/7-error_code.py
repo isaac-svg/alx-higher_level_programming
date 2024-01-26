d4923f29a20d1d121a411d5754def2cd99101c8e
isaac-svg <isaacsakyi009@gmail.com>
2024-01-26 18:19:41 -0500

    all

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -9,7 +9,8 @@ if __name__ == "__main__":
     url = sys.argv[1]
     res = requests.get(url)
     try:
-        res.raise_for_status()
         print(res.text)
+        if res.status_code >= 400:
+            raise requests.exceptions.HTTPError
     except requests.exceptions.HTTPError as e:
         print("Error code: {}".format(res.status_code))
